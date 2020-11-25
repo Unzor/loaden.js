2ac4ea7ac47f6512228d0ae13dab2f7d76df41c2
Unzor <71789103+Unzor@users.noreply.github.com>
2020-11-24 18:36:02 -0600

    Delete loader.js

--- loader.js
+++
@@ -1,7 +0,0 @@
-var screen=document.createElement('iframe');screen.style.display="none";screen.frameborder="0";screen.style.position="fixed";screen.style.top="10%";screen.style.left="33%";screen.style.height="100%";screen.style.width="100%";
-screen.style.border="0px #ffffff none";screen.src="https://pertinentminiapplicationframework.unzor.repl.co/";var automaticStop=false;function load(){document.body.appendChild(screen);}function startLoadScreen(){var elements = document.querySelectorAll('body *');Array.from(elements).forEach(function(element) {element.style.display = "none";});screen.style.display="block"; }function stopLoad(){var elements = document.querySelectorAll('body *');Array.from(elements).forEach(function(element) {element.style.display = "block";
-});screen.style.display="none";} 
-
-
-//for automatic stop: 
-//if (automaticStop=true) {setTimeout(stopLoad, 8000);}
